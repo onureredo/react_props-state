33ac66370b841642c0df67f289890095a1385d19
Onur Eredo <onureredo@yahoo.com>
2022-10-30 23:01:26 +0100

    added buttons

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import minion from './images/minion.png';
 
 
 function App() {
+ 
   return (
     <div className="App">
       <h1>AMAZON PRIME SALE</h1>
-            
+
       <Amazonproduct
         title="Amazon Echo"
         description="Your Home Assistant"
@@ -19,21 +20,21 @@ function App() {
         price={74.99}
       />
 
-        <Amazonproduct
+      <Amazonproduct
         title="Amazon Fire Stick 4K"
         description="Stream in 4K"
         image={firestick}
         price={59.99}
       />
 
-        <Amazonproduct
+      <Amazonproduct
         title="Amazon Fire HD 10' Tablet"
         description="12 Hours Battery"
         image={firehd}
         price={100.99}
       />
 
-        <Anothercomponent 
+      <Anothercomponent 
         title="Employee of the Month"
         name="Joey"
         job="JR Web-Developer"
@@ -41,6 +42,21 @@ function App() {
         comment="Joey's comment: I try to be Creative..."
       />
 
+      <button 
+          onClick={() => {
+            window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+          }}
+          id="btn_top" 
+          title="Scroll to Top">▲
+      </button>
+      <button 
+          onClick={() => {
+            window.scrollTo({top: 5000, left: 0, behavior: 'smooth'});
+          }}
+          id="btn_down" 
+          title="Scroll to Bottom">▼
+      </button>
+    
     </div>
   );
 }

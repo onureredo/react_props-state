ce634f273f507b4ebe695805e866092f3136a4bf
Onur Eredo <onureredo@yahoo.com>
2022-10-30 23:07:12 +0100

    edits in main comp

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
  
   return (
     <div className="App">
-      <h1>AMAZON PRIME SALE</h1>
+      <h1>AMAZON PRIME SALE !</h1>
 
       <Amazonproduct
         title="Amazon Echo"

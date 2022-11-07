a0e9be94a79fc84398f12476bdae2fe43da8b12c
Onur Eredo <onureredo@yahoo.com>
2022-11-07 18:01:24 +0100

    changes

--- src/components/Team.js
+++ src/components/Team.js
@@ -9,17 +9,6 @@ function Team() {
         comment: `" Let's code together 💻" `
     });
 
-    //Prev state
-    const prev = () => {
-        setTeam({
-            title: "Meet Our Team",
-            name: "Bob",
-            image: "/images/bob.png",
-            job: "Junior Web Developer",
-            comment: `" Let's code together 💻" `
-        });
-      };
-
     const next = () => {
         setTeam(previousState => {
             return {...previousState,                
@@ -33,15 +22,15 @@ function Team() {
 
   return (
     <div className='team_container'>
-        <button onClick={prev} id="btn_show">◀</button>
+        
         <div className='person'>
             <h1>{team.title}</h1>
             <h2>{team.name}</h2>
-            <img src={team.image} alt=""/>
+            <img src={team.image} id={team.name.toLowerCase()} alt=""/>
             <h2>{team.job}</h2>
             <h3>{team.comment}</h3>
         </div>
-        <button onClick={next} id="btn_show">▶</button>
+        <button onClick={next} id="btn_show">NEXT</button>
     </div>
   )
 }
